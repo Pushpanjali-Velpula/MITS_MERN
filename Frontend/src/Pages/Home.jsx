142710766a863955a90c692a5c75c313fdec2336
Pushpa <pushpavelpula630@gmail.com>
2026-01-27 16:35:01 +0530

    First

---
+++ Frontend/src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+const Home = ({name}) => {
+  const navigate=useNavigate();
+  const handleClick=()=>{
+    navigate('/About')
+  }
+  return (
+    <div>
+        <h1>Name:{name}</h1>
+        <button onClick={handleClick}>Go to About page</button>
+    </div>
+  )
+}
+
+export default Home
---
+++ Frontend/src/component/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Navbar.css'
+import {Link} from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav>
+            <ul className="navbar">
+                <li><Link className='Link' to="/">Home</Link></li>
+                <li><Link className='Link' to="/About">About</Link></li>
+                <li><Link className='Link' to="/Contact">Contact</Link></li>
+                <li><Link className='Link' to="/Service">Service</Link></li>
+                <li><Link className='Link' to="/login">Login</Link></li>
+                <li><Link className='Link' to="/hook">Hook</Link></li>
+                <li><Link className='Link' to="/Signup">Signup</Link></li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
